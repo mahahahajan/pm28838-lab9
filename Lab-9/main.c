4e4487f3cc74ab869eafc8bf7539e3244d3ca63a
Devangshu Goswami <devgos4@gmail.com>
2021-11-07 19:23:29 -0600

    UART reads string and puts in FIFO buffer

--- Lab-9/main.c
+++ Lab-9/main.c
@@ -2,6 +2,13 @@
 #include "../inc/PLL.h"
 #include "../inc/CortexM.h"
 #include "../inc/UART0int.h"
+#include "../inc/FIFO.h"
+
+#define SIZE 1024
+#define SUCCESS 1
+#define FAIL 0
+
+AddPointerFifo(Raw, SIZE, char, SUCCESS, FAIL)
 
 //---------------------OutCRLF---------------------
 // Output a CR,LF to UART to go to a new line
@@ -13,31 +20,55 @@ void OutCRLF(void){
 }
 //debug code
 int main(void){
-  char i;
-  char string[20];  // global to assist in debugging
-
-
   PLL_Init(Bus80MHz);       // set system clock to 50 MHz
   UART_Init();              // initialize UART
+  UART_OutString(" UART0 is ready to use!"); OutCRLF();
   EnableInterrupts();       // Enable interrupts
-  OutCRLF();
-  for(i='A'; i<='Z'; i=i+1){// print the uppercase alphabet
-    UART_OutChar(i);
+  RawFifo_Init();
+
+  char string[20];  // global to assist in debugging
+  char recovered_String[20*5];
+
+  int i;
+  for(i = 0; i < 5; i++){
+      UART_OutString("InString: ");
+          UART_InString(string,19);
+          UART_OutString(" OutString="); UART_OutString(string);OutCRLF();
+
+          // Store string in FIFO buffer
+          int j = 0;
+          while(string[j] != 0){
+              RawFifo_Put(string[j]);
+              j++;
+          }
+          RawFifo_Put(0);
   }
-  OutCRLF();
-  UART_OutChar(' ');
-  for(i='a'; i<='z'; i=i+1){// print the lowercase alphabet
-    UART_OutChar(i);
+
+  while(RawFifo_Size() != 0){
+      static int i = 0;
+      char read_char = 0;
+      RawFifo_Get(&read_char);
+      UART_OutChar(read_char);
+      recovered_String[i] = read_char;
+      i++;
   }
-  OutCRLF();
-  UART_OutChar('-');
-  UART_OutChar('-');
-  UART_OutChar('>');
+
   while(1){
+
+  }
+
+  // Print out contents of FIFO buffer;
+
+  /*
+  while(1){
+    // Input user data
     UART_OutString("InString: ");
     UART_InString(string,19);
     UART_OutString(" OutString="); UART_OutString(string); OutCRLF();
 
+    // Store string in FIFO buffer
+
   }
+  */
 }
 
