f54ea8e0dca325314323f3318e7c5541dd07a31a
Anuv <anuv.ca@gmail.com>
2021-11-09 12:17:31 -0600

    update transmitter project to work with keil as well as ccs

--- Lab-9-Transmitter/Main.h
+++ Lab-9-Transmitter/Main.h
@@ -3,12 +3,12 @@
 
 
 #include <stdint.h>
-#include "../inc/PLL.h"
-#include "../inc/CortexM.h"
-#include "../inc/UART0int.h"
-#include "../inc/FIFO.h"
-#include "../inc/tm4c123gh6pm.h"
-#include "../inc/LaunchPad.h"
+#include "inc/PLL.h"
+#include "inc/CortexM.h"
+#include "inc/UART0int.h"
+#include "inc/FIFO.h"
+#include "inc/tm4c123gh6pm.h"
+#include "inc/LaunchPad.h"
 
 #include "TimerDriver.h"
 #include "Transmitter.h"
